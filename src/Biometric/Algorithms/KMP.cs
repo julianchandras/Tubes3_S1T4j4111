2b6e02d3a4bb28c406df203ed4eb28fa0bbd8498
Julian <julianchandrasutadi@gmail.com>
2024-05-22 21:45:00 +0700

    reinitialize project using WPF

---
+++ src/Biometric/Algorithms/KMP.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biometric.Algorithms
+{
+    class KMP
+    {
+        public static int KMPSearch(string text, string pattern)
+        {
+            int n = text.Length;
+            int m = pattern.Length;
+            int[] b = ComputeBorder(pattern);
+            int i = 0;
+            int j = 0;
+
+            while (i < n)
+            {
+                if (pattern[j] == text[i])
+                {
+                    if (j == m - 1)
+                        return i - m + 1;
+                    i++;
+                    j++;
+                }
+                else if (j > 0)
+                {
+                    j = b[j - 1];
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return -1;
+        }
+
+        public static int[] ComputeBorder(string pattern)
+        {
+            int[] b = new int[pattern.Length];
+            b[0] = 0;
+            int m = pattern.Length;
+            int j = 0;
+            int i = 1;
+
+            while (i < m)
+            {
+                if (pattern[i] == pattern[j])
+                {
+                    b[i] = j + 1;
+                    i++;
+                    j++;
+                }
+                else if (j > 0)
+                {
+                    j = b[j - 1];
+                }
+                else
+                {
+                    b[i] = 0;
+                    i++;
+                }
+            }
+            return b;
+        }
+    }
+}
---
+++ src/Biometric/Algorithms/RegularExpression.cs
@@ -0,0 +1,82 @@
+﻿using Biometric.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biometric.Algorithms
+{
+    class RegularExpression
+    {
+        private static string[] word = new string[26]
+        {
+            "(aA4)?",
+            "[bB8]",
+            "[cC]",
+            "[dD]",
+            "([eE3])?",
+            "[fF]",
+            "[gG6]",
+            "[hH]",
+            "([iI1])?",
+            "[jJ]",
+            "[kK]",
+            "[lL]",
+            "[mM(111)]",
+            "[nN(11)]",
+            "([oO0])?",
+            "[pP]",
+            "[qQ]",
+            "[rR(12)]",
+            "[sS5]",
+            "[tT]",
+            "([uU])?",
+            "[vV]",
+            "[wW]",
+            "[xX]",
+            "[yY]",
+            "[zZ2]"
+        };
+
+        private string regexText;
+
+        private string findRegex(char c)
+        {
+            int idx = ((int)c) % 65;
+            return RegularExpression.word[idx];
+        }
+
+        public RegularExpression(string text)
+        {
+            text = text.ToUpper();
+            regexText = @"";
+            foreach (char character in text)
+            {
+                if (character != ' ')
+                {
+                    regexText += findRegex(character);
+                }
+                else
+                {
+                    regexText += "\\s";
+                }
+            }
+        }
+
+        public string GetRegexText()
+        {
+            return regexText;
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Instantiate RegularExpression and print the result
+            RegularExpression re = new("ayam");
+            Console.WriteLine(re.GetRegexText());
+        }
+    }
+}

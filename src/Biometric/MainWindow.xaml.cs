ba041cfed8300141d1fe7dc61755b2d2a674d541
Bryan Cornelius Lauwrence <bryan.cornelius.lauw@gmail.com>
2024-05-25 01:04:39 +0700

    GUI bang

--- src/Biometric/MainWindow.xaml.cs
+++ src/Biometric/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +21,26 @@ namespace Biometric
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    // Load the selected image file into the image control
+                    BitmapImage bitmapImage = new BitmapImage(new Uri(openFileDialog.FileName));
+                    imageControl.Source = bitmapImage;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading image: " + ex.Message);
+                }
+            }
         }
     }
 }

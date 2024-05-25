a0caadb765bef769e2dee545d16162e611041e4a
Julian <julianchandrasutadi@gmail.com>
2024-05-25 11:22:32 +0700

    added testing for db connection InitializeAsync

--- src/Biometric/MainWindow.xaml.cs
+++ src/Biometric/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.Win32;
+﻿using Biometric.Models;
+using Biometric.Repository;
+using DotNetEnv;
+using Microsoft.Win32;
+using MySqlConnector;
 using System;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,6 +26,58 @@ namespace Biometric
         public MainWindow()
         {
             InitializeComponent();
+
+            _ = InitializeAsync();
+        }
+
+        // Use an asynchronous method to perform initialization
+        private async Task InitializeAsync()
+        {
+            MessageBox.Show(Directory.GetCurrentDirectory());
+
+            Env.Load("..\\..\\.env");
+
+            string server = Env.GetString("DB_SERVER");
+            string user = Env.GetString("DB_USER");
+            string password = Env.GetString("DB_PASS");
+            string database = Env.GetString("DB_DATABASE");
+
+            string connectionString = $"Server={server};User={user};Password={password};Database={database};";
+
+            MessageBox.Show(connectionString);
+
+            // Execute the query using Dapper
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                // Step 1: Create an instance of PersonRepository
+                PersonRepository pr = new PersonRepository(connectionString);
+
+                // Step 2: Create a new Person object with the required details
+                Person test = new Person
+                {
+                    NIK = "13522080",  // Assuming this is the NIK
+                    Nama = "John Doe",
+                    TempatLahir = "Jakarta",
+                    TanggalLahir = new DateTime(1990, 1, 1), // Example date of birth
+                    JenisKelamin = "Laki-Laki",
+                    GolonganDarah = "O",
+                    Alamat = "Jl. Sudirman No. 10",
+                    Agama = "Islam",
+                    StatusPerkawinan = "Belum Menikah",
+                    Pekerjaan = "Developer",
+                    Kewarganegaraan = "Indonesia",
+                };
+                MessageBox.Show(test.ToString());
+
+                // Step 3: Call InsertPersonAsync method to insert the new person
+                int rowsAffected = await pr.InsertPersonAsync(test);
+
+                MessageBox.Show(rowsAffected.ToString());
+                IEnumerable<Person> allPersons = await pr.GetAllPersonsAsync();
+
+                // Convert rowsAffected to string before showing it in MessageBox
+                MessageBox.Show(((allPersons.First()).NIK).ToString());
+            }
         }
 
         private void ImageButton_Click(object sender, RoutedEventArgs e)
